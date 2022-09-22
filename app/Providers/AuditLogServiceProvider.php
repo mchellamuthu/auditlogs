0d04b4ff81ce38b6617cf75d8712bdeb911ec1c7
Chella <mchellamuthu@live.com>
2022-09-22 10:29:49 +0530

    added audit logs provider

---
+++ app/Providers/AuditLogServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuditLogServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    public function configureModels()
+    {
+
+    }
+
+
+}
